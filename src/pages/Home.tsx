85f9c0deeeac690e444abc2357019bce1dcc1806
Paulo Vitor <paulovitor2123@gmail.com>
2022-02-08 21:51:36 -0300

    Adding, remove and edit tasks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,37 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks(tasksBefore =>
+      [
+        ...tasksBefore,
+        {
+          title: newTaskTitle,
+          id: new Date().getTime(),
+          done: false
+        }
+      ]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const editableTask = tasks.find(task => task.id === id);
+    const indexPosition = tasks.findIndex(task => task.id === id);
+    const newTask = {
+      id: editableTask?.id,
+      title: editableTask?.title,
+      done: !editableTask?.done
+    } as Task;
+    const newTasks = [...tasks.filter(task => task.id !== id)];
+    newTasks.splice(indexPosition, 0, newTask);
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasksBefore => tasksBefore.filter(task => task.id !== id));
   }
 
   return (
@@ -26,10 +44,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
